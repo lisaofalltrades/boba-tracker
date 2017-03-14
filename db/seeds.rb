5d1cb225910cdf429e45c6fbee5da39af045a934
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-03-14 11:18:34 -0700

    seed files draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sj_boba = Restaurant.create([
+  {name: 'Tea Lyfe Drinks',
+  address: '989 Story Rd, San Jose, CA 95122'},
+  {name: 'Passion-T Snacks and Desserts',
+  address: '2266 Senter Rd, San Jose, CA 95112'},
+  {name: 'Pop Up Tea, Coffee & Snacks',
+  address: '2266 Senter Rd, San Jose, CA 95112'},
+  {name: 'Pekoe',
+  address: '3276 S White Rd, San Jose, CA 95148'},
+  {name: 'Shincha Tea',
+  address: '2487 Alvin Ave, San Jose, CA 95121'}
+  ])
